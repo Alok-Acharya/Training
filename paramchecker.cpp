5f984182fabddc7b5d2ecef53046042b1e329e2c
Alok-Acharya <68507354+Alok-Acharya@users.noreply.github.com>
2020-07-21 15:31:46 +0530

    FIx cyclometric-complexity

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,11 +1,27 @@
-
-bool vitalsAreOk(float bpm, float spo2, float respRate) {
+bool checkbpm(float)
+{
   if(bpm < 70 || bpm > 150) {
-    return false;
-  } else if(spo2 < 80) {
-    return false;
-  } else if(respRate < 30 || respRate > 60) {
     return false;
   }
   return true;
+}
+bool checkspo2(float)
+{
+  if(spo2 < 80) {
+    return false;
+  }
+  return true;
+}
+bool checkrespRate(float)
+{
+  if(respRate < 30 || respRate > 60) {
+    return false;
+  }
+  return true;
+}
+bool vitalsAreOk(float bpm, float spo2, float respRate) {
+  if(checkbpm(bpm)&&checkspo2(sop2)&&checkrespRate(respRate)){
+  return true;
+  }
+  return false;
 }
