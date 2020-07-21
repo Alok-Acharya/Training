32fd130b21e340db1b79ddfecddfb190f431ef07
Alok-Acharya <68507354+Alok-Acharya@users.noreply.github.com>
2020-07-21 22:45:29 +0530

    FixBuildError

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -9,10 +9,10 @@ bool paramChecker(float param , float lowerlimit , float higherlimit)
 }
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  bool bpm = paramChecker(bpm,70,150);
-  bool spo2 = paramchecker(spo2,80,100);
-  bool resprate = paramchecker(respRate,30,60);
-  if(bpm && spo2 && resprate){
+  bool bpmOK = paramChecker(bpm,70,150);
+  bool spo2OK = paramChecker(spo2,80,100);
+  bool resprateOK = paramChecker(respRate,30,60);
+  if(bpmOK && spo2OK && resprateOK){
       return true;
   }
   return false; 
