c79f1702975b05e3f4831c351244be4f0b5100d1
Alok-Acharya <68507354+Alok-Acharya@users.noreply.github.com>
2020-07-22 14:54:36 +0530

    UpdatedParamCheckerFunction

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,19 +1,17 @@
 
 bool paramChecker(float param , float lowerlimit , float higherlimit)
 {
-   if(param < lowerlimit || param > higherlimit)
-   {
-      return false;
-   }
-   return true;
+   return(!(param < lowerlimit || param > higherlimit));
 }
 
+
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  bool bpmOK = paramChecker(bpm,70,150);
-  bool spo2OK = paramChecker(spo2,80,100);
-  bool resprateOK = paramChecker(respRate,30,60);
-  if(bpmOK && spo2OK && resprateOK){
+   bool bpmOK = paramChecker(bpm,70,150);
+   bool spo2OK = paramChecker(spo2,80,100);
+   bool resprateOK = paramChecker(respRate,30,60);
+  
+   if(bpmOK && spo2OK && resprateOK){
       return true;
-  }
-  return false; 
+   }
+   return false; 
 }
