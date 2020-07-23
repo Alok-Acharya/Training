8226b706d2c95d2b32935fb69e2926b51cf9ed63
Alok-Acharya <68507354+Alok-Acharya@users.noreply.github.com>
2020-07-23 23:46:40 +0530

    Supporting multiple param(OpenClosePrinciple)

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,15 +1,62 @@
 
-bool paramChecker(float param , float lowerlimit , float higherlimit)
+
+float param;
+float lowerlimit;
+float upperlimit;
+class paramChecker
 {
-   return(!(param < lowerlimit || param > higherlimit));
-}
+	public:
+	paramChecker(){
+	virtual ~paramChecker(){}
+	bool IsParamOK()
+	{
+		return(!(param < lowerlimit || param > upperlimit));
+	}		
+};
+
+class Bpm : public paramChecker
+{
+   public:
+   Bpm(float bpm)
+   {
+	 param = bpm;
+	 lowerlimit = 70;
+	 upperlimit = 150;
+   }
+   virtual ~Bpm(){}
+};
+
+class Spo2 : public paramChecker
+{
+   public:
+   Spo2(float spo2)
+   {
+	 param = spo2;
+	 lowerlimit = 80;
+	 upperlimit = 100;
+   }
+   virtual ~Spo2(){}
+};
+
+class RespRate : public paramChecker
+{
+   public:
+   RespRate(float resprate)
+   {
+	 param = resprate;
+	 lowerlimit = 30;
+	 upperlimit = 60;
+   }
+	virtual ~RespRate(){}
+};
 
 
-bool vitalsAreOk(float bpm, float spo2, float respRate) {
-   bool bpmOK = paramChecker(bpm,70,150);
-   bool spo2OK = paramChecker(spo2,80,100);
-   bool resprateOK = paramChecker(respRate,30,60);
-  
-   return(bpmOK && spo2OK && resprateOK);
- 
+bool vitalsAreOk(vector<float>parm) {
+   paramChecker *obj1 = new Bpm(parm[0]);
+	bool bpmOk = obj1->IsParamOK();
+	paramChecker *obj2 = new Spo2(parm[1]);
+   bool spo2OK = obj2->IsParamOK();
+   paramChecker *obj3 = new RespRate(parm[2]);
+	bool resprateOK = obj3->IsParamOK();
+	return (bpmOk && Spo2 && resprate); 
 }
