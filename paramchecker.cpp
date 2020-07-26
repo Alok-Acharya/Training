9302e75100e70ed8ee60949c37a474e3b4dc1ea6
Alok-Acharya <68507354+Alok-Acharya@users.noreply.github.com>
2020-07-26 20:43:13 +0530

    Update paramchecker.cpp

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,27 +1,33 @@
 #include "paramchecker.h"
 using namespace std;
-float param;
-float lowerlimit;
-float upperlimit;
+#include <iostream>
+#include <vector>
+using namespace std;
 class paramChecker
 {
+	private:
+		float lower_limit;
+		float upper_limit;
 	public:
-	paramChecker(){}
-	virtual ~paramChecker(){}
-	bool IsParamOK()
-	{
-		return(!(param < lowerlimit || param > upperlimit));
-	}		
+		paramChecker(){lower_limit = 0; upper_limit = 0;}
+		virtual ~paramChecker(){}
+		void setparamlimits(float lowerlimit , float upperlimit)
+		{
+			lower_limit = lowerlimit;
+			upper_limit = upperlimit;
+		}
+		bool IsParamOK(float param)
+		{
+			return(!(param < lower_limit || param > upper_limit));
+		}		
 };
 
 class Bpm : public paramChecker
 {
    public:
-   explicit Bpm(float bpm)
+   Bpm()
    {
-	 param = bpm;
-	 lowerlimit = 70;
-	 upperlimit = 150;
+     setparamlimits(70,150);
    }
    virtual ~Bpm(){}
 };
@@ -29,11 +35,9 @@ class Bpm : public paramChecker
 class Spo2 : public paramChecker
 {
    public:
-   explicit Spo2(float spo2)
+   Spo2()
    {
-	 param = spo2;
-	 lowerlimit = 80;
-	 upperlimit = 100;
+     setparamlimits(80,100);
    }
    virtual ~Spo2(){}
 };
@@ -41,22 +45,47 @@ class Spo2 : public paramChecker
 class RespRate : public paramChecker
 {
    public:
-   explicit RespRate(float resprate)
+   RespRate()
    {
-	 param = resprate;
-	 lowerlimit = 30;
-	 upperlimit = 60;
+     setparamlimits(30,60);
    }
-	virtual ~RespRate(){}
+   virtual ~RespRate(){}
+};
+
+
+class Vital
+{
+	std::vector<paramChecker*> params;
+	public:
+	Vital()
+	{
+		paramChecker *bpm = new Bpm();
+		paramChecker *spo2 = new Spo2();
+		paramChecker *resp = new RespRate();
+		params.push_back(bpm);
+		params.push_back(spo2);
+		params.push_back(resp);
+	}
+	bool IsVitalsOutOfRange(std::vector<float> vitals)
+	{
+		std::vector<paramChecker*>::iterator Itr = params.begin();
+		std::vector<float>::iterator ItrVital = vitals.begin();
+	        bool IsVitalsOutOfRange = false;
+		for (; Itr != params.end() ; ++Itr, ++ItrVital)
+		{
+			if(!(*Itr)->IsParamOK(*ItrVital))
+			{
+				IsVitalsOutOfRange = true;
+				break;
+			}
+		}
+		return IsVitalsOutOfRange;
+	}
+	virtual ~Vital(){}
 };
 
 
 bool vitalsAreOk(std::vector< float >parm) {
-   paramChecker *obj1 = new Bpm(parm[0]);
-   bool bpmOk = obj1->IsParamOK();
-   paramChecker *obj2 = new Spo2(parm[1]);
-   bool spo2OK = obj2->IsParamOK();
-   paramChecker *obj3 = new RespRate(parm[2]);
-   bool resprateOK = obj3->IsParamOK();
-   return (bpmOk && spo2OK && resprateOK); 
+   Vital VitalsOK;
+   return !(VitalsOK.IsVitalsOutOfRange(parm)); 
 }
