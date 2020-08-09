cf9766295ea963d3b8e5585b5ae5437311fde787
Alok-Acharya <68507354+Alok-Acharya@users.noreply.github.com>
2020-08-09 17:29:24 +0530

    Added file to Monitor PartDimension

---
+++ PartDimension.h
@@ -0,0 +1,23 @@
+#ifndef _PART_DIMENSION_H_
+#define _PART_DIMENSION_H_
+
+
+#include<iostream>
+#include"IValidator.h"
+#include "MachineFailureNotification.h"
+using namespace std;
+
+#define Part_Dimension_Variation_Threshold 0.05 //in mm
+
+class PartDimension
+{
+    public:
+	PartDimension();
+    virtual ~PartDimension();
+	void validatePartDimensionVariation(float);
+    private:
+       INotify *machinefailure;
+	   IValidator *validatePartDimension;
+
+};
+#endif
