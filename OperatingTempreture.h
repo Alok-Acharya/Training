145ef3f0f13906e478a58e12cabb4e9abe1fbc0d
Alok-Acharya <68507354+Alok-Acharya@users.noreply.github.com>
2020-08-09 17:34:12 +0530

    Added file to Monitor Tempreture

---
+++ OperatingTempreture.h
@@ -0,0 +1,24 @@
+#ifndef _OPERATINGH_TEMPRETURE_H_
+#define _OPERATINGH_TEMPRETURE_H_
+
+#define Operating_Tempreture_Threshold 35 //in degree
+
+#include<iostream>
+#include "IValidator.h"
+#include "EnviornmentFailureNotification.h"
+using namespace std;
+
+
+
+class OperatingTempreture:public IValidator
+{
+    public:
+	OperatingTempreture();
+    virtual ~OperatingTempreture();
+	void validateOperatingTempreture(float);
+    private:
+       INotify *enviornmentfailure;
+	   IValidator *validatetempreture;
+
+};
+#endif
