099581ec8d3c6a48df7d98efc862884a4a349da0
Alok-Acharya <68507354+Alok-Acharya@users.noreply.github.com>
2020-08-09 17:36:56 +0530

    Added file to Monitor Operation Duration

---
+++ OperationDuration.h
@@ -0,0 +1,27 @@
+#ifndef _OPERATION_DURATION_H_
+#define _OPERATION_DURATION_H_
+
+#define Operation_Duration_Threshold 6 //in minutes
+#include<iostream>
+#include "MachineFailureNotification.h"
+#include "IValidator.h"
+
+using namespace std;
+
+bool IsDataOK(float data , float thresholdvalue)
+{
+	return(!(data > thresholdvalue));		
+}
+
+class OperationDuration
+{
+    public:
+	OperationDuration();
+    virtual ~OperationDuration();
+	void checkContineousOperationDuration(float);
+    private:
+	 INotify* machinefailure;
+	 IValidator* validateDuration;
+
+};
+#endif
