cc81621d8e89a51f5bfda779ec8dc56bf370dffc
Alok-Acharya <68507354+Alok-Acharya@users.noreply.github.com>
2020-08-11 23:13:59 +0530

    Added function to get Part dimension Variation

--- PartDimension.cpp
+++ PartDimension.cpp
@@ -9,7 +9,11 @@ PartDimension::~PartDimension()
 {
       delete machinefailure;
 }
-		 
+void PartDimension::handlePartDimensionVariationUpdate(float dimensionvariation)
+{
+    validatePartDimensionVariation(dimensionvariation);
+}
+
 bool PartDimension::validatePartDimensionVariation(float dimensionVariation)
 {	IValidator validatePartDimension;
 	if(validatePartDimension.IsDataOK(dimensionVariation, Part_Dimension_Variation_Threshold))
