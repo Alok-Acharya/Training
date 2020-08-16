bc54c51cb0033b37841a98de1ffb39d602937827
Alok-Acharya <68507354+Alok-Acharya@users.noreply.github.com>
2020-08-16 23:27:19 +0530

    Update and rename PartDimension.h to CNCPartDimensionMonitor.h

---
+++ CNCPartDimensionMonitor.h
@@ -0,0 +1,26 @@
+#ifndef _PART_DIMENSION_MONITOR_H_
+#define _PART_DIMENSION_MONITOR_H_
+
+
+#include<iostream>
+#include "IValidator.h"
+#include "CNCMachineHealth.h"
+#include "MachineFailureNotification.h"
+
+
+using namespace std;
+
+class CNCPartDimensionMonitor
+{
+    public:
+	CNCPartDimensionMonitor(IValidator* , INotify* , CNCMachineHealth*);
+        virtual ~CNCPartDimensionMonitor();
+	void CNCPartDimensionMonitor(float);
+	float GetPartDimensionVariation();
+    private:
+	 float _dimVariation;
+	 IValidator* _validate;
+         INotify* _notify;
+	 CNCMachineHealth* _cnchealth;
+};
+#endif
