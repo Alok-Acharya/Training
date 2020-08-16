20b68c0cf2022e9e34a551f79eb613abf5da2b4f
Alok-Acharya <68507354+Alok-Acharya@users.noreply.github.com>
2020-08-17 00:42:15 +0530

    Added file to validateself test code

---
+++ CNCSelfTestValidator.h
@@ -0,0 +1,19 @@
+#ifndef _CNC_SELFTEST_VALIDATOR_
+#define _CNC_SELFTEST_VALIDATOR_
+
+#include<iostream>
+#include "CNCDefines.h"
+#include "DignosisFailureNotification.h"
+
+using namespace std;
+ 
+class CNCSelfTestValidator
+{
+    public:
+	CNCSelfTestValidator(INotify*);
+    virtual ~CNCSelfTestValidator();
+	bool validate(unsigned int);
+	private:
+	 INotify* _notify;
+};
+#endif
