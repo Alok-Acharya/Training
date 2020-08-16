7373c9a67cf572584cb895b1ed3e0c55a23c3d4c
Alok-Acharya <68507354+Alok-Acharya@users.noreply.github.com>
2020-08-17 00:39:54 +0530

    Update and rename SelfTestStatus.h to CNCSelfTestStatusMonitor.h

---
+++ CNCSelfTestStatusMonitor.h
@@ -0,0 +1,22 @@
+#ifndef _CNC_SELFTEST_STATUS_MONITOR_
+#define _CNC_SELFTEST_STATUS_MONITOR_
+
+#include<iostream>
+#include "CNCMachineHealth.h"
+#include "CNCSelfTestValidator.h"
+
+using namespace std;
+ 
+class CNCSelfTestStatusMonitor
+{
+    public:
+	CNCSelfTestStatusMonitor(CNCSelfTestValidator* , CNCMachineHealth* );
+    virtual ~CNCSelfTestStatusMonitor();
+	void SelftestStatusUpdate(unsigned int );
+	CNC_StatusCode GetStatusCode();
+    private:
+	unsigned int _statuscode;
+	CNCSelfTestValidator *_validator;
+	CNCMachineHealth* _cnchealth;
+};
+#endif
