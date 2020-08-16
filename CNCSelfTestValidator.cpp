d1fdbab544a19ca8efe5e162c45aa2852cc5eb6e
Alok-Acharya <68507354+Alok-Acharya@users.noreply.github.com>
2020-08-17 00:43:04 +0530

    Create CNCSelfTestValidator.cpp

---
+++ CNCSelfTestValidator.cpp
@@ -0,0 +1,27 @@
+#include "CNCSelfTestValidator.h"
+
+
+CNCSelfTestValidator::CNCSelfTestValidator(INotify* notify):_notify(notify)
+{
+	
+}
+CNCSelfTestValidator::~CNCSelfTestValidator()
+{
+	
+	
+}
+
+bool CNCSelfTestValidator::validate(unsigned int code)
+{
+	if(ALL_OK == code)
+	{
+		cout << "Machine ALL OK";
+		return true;
+	}
+	else
+	{
+		_notify->Notification();
+		return true;
+	}
+	
+}
