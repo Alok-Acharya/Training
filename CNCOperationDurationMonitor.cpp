81940cd8be816ab86b97c550ecc72d427a6156b7
Alok-Acharya <68507354+Alok-Acharya@users.noreply.github.com>
2020-08-17 00:09:04 +0530

    Update and rename OperationDuration.cpp to CNCOperationDurationMonitor.cpp

---
+++ CNCOperationDurationMonitor.cpp
@@ -0,0 +1,37 @@
+
+#include "CNCOperationDurationMonitor.h"
+
+CNCOperationDurationMonitor::CNCOperationDurationMonitor(IValidator* validate, INotify* notify , CNCMachineHealth* health)
+{
+	_validate = validate;
+	_notify = notify;
+	_cnchealth = health;
+	_durationinminute = 0;
+
+}
+
+
+CNCOperationDurationMonitor::~CNCOperationDurationMonitor()
+{
+
+}
+
+void CNCOperationDurationMonitor::OperationDurationUpdate(float duration)
+{
+	_durationinminute = duration;
+	if(!(_validate->ValidateData(_durationinminute,Operation_Duration_Threshold)))
+	{
+		_notify->Notification();
+		_cnchealth->updateCNCHealth(Machine_Failure);
+	}
+	else
+	{
+		_cnchealth->updateCNCHealth(NO_Failure);
+	}
+}
+
+float OperationDurationUpdate::GetOperationDuraion(float duration)
+{
+	return _durationinminute;
+}
+
