f82907bd00d70d5b52ed3e6238b3af3840ab544e
Alok-Acharya <68507354+Alok-Acharya@users.noreply.github.com>
2020-08-16 22:41:42 +0530

    Added new file to store CNCHealth

---
+++ CNCMachineHealth.cpp
@@ -0,0 +1,21 @@
+#include "CNCMachineHealth.h"
+
+CNCMachineHealth::CNCMachineHealth()
+{
+	_cncCondition = NO_Failure;
+}
+
+CNCMachineHealth::~CNCMachineHealth()
+{
+	
+}
+
+void CNCMachineHealth::updateCNCHealth(CNC_Health health)
+{
+	_cncCondition = health;
+}
+
+CNC_Health CNCMachineHealth::GetCNCHealth()
+{
+	return _cncCondition;
+}
